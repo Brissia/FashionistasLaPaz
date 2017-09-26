c1ca4f13bc57e7d50d3cce718549ad9e3eb00058
Brissia Benavente <brissia.benavente@gmail.com>
2017-09-25 21:31:51 -0400

    Welcome Label

--- FashionistasLaPaz/ViewController.swift
+++ FashionistasLaPaz/ViewController.swift
@@ -8,14 +8,21 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
 
-    override func viewDidLoad() {
+    @IBOutlet weak var welcomeLabel: UILabel!
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.welcomeLabel.text = "Bienvenida Fashionista"
     }
 
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
