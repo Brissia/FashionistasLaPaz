3ed83d462e5824a1f6afd1db700e6871b96fdbd5
Brissia Benavente <brissia.benavente@gmail.com>
2017-09-28 20:14:38 -0400

    Fix sign in action

--- FashionistasLaPaz/ViewController.swift
+++ FashionistasLaPaz/ViewController.swift
@@ -27,6 +27,9 @@ class ViewController: UIViewController
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func signIn(_ sender: Any) {
+        print("Sign In Presionado")
+    }
+    
 }
 
